8ce0cebc92097bacd11a19561b64acf88501feb6
arvinth-ravi <77610973+arvinth-ravi@users.noreply.github.com>
2021-01-28 19:55:16 +0530

    Init changes
    Min max calculations

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,20 @@
 #include "stats.h"
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
-    //Implement statistics here
+using namespace Statistics;
+
+Stats Statistics::ComputeStatistics(const std::vector<float>& vect) {
+
+	std::vector<float>::const_iterator itr = vect.begin();
+	Stats S1();
+	float t = 0.0F;
+    S1.min = *min_element(vect.begin(),vect.end());
+    S1.max = *max_element(vect.begin(),vect.end());
+
+		for( ; itr!=vect.end(); ++itr)
+		{
+			t = t + *itr;
+		}
+
+		S1.avg = t / t.size() ;
+		return S1;
 }
