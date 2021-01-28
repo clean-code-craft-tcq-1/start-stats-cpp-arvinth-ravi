2cb4d618a9c3cc75d3a515f88757155583c6da6a
arvinth-ravi <77610973+arvinth-ravi@users.noreply.github.com>
2021-01-28 20:20:56 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -7,8 +7,8 @@ Stats Statistics::ComputeStatistics(const std::vector<float>& vect) {
 	std::vector<float>::const_iterator itr = vect.begin();
 	Stats S1;
 	float t = 0.0F;
-    S1.min = std::min_element(vect.begin(),vect.end());
-    S1.max = std::max_element(vect.begin(),vect.end());
+    S1.min = *std::min_element(vect.begin(),vect.end());
+    S1.max = *std::max_element(vect.begin(),vect.end());
 
 		for( ; itr!=vect.end(); ++itr)
 		{
