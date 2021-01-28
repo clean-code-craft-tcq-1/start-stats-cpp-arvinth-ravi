f916e03ff4f68151421377543645d18a9a304e30
arvinth-ravi <77610973+arvinth-ravi@users.noreply.github.com>
2021-01-28 20:08:20 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -5,7 +5,7 @@ using namespace Statistics;
 Stats Statistics::ComputeStatistics(const std::vector<float>& vect) {
 
 	std::vector<float>::const_iterator itr = vect.begin();
-	Stats S1();
+	Stats S1;
 	float t = 0.0F;
     S1.min = *min_element(vect.begin(),vect.end());
     S1.max = *max_element(vect.begin(),vect.end());
